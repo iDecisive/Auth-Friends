ece445ba5266681add40d14a9810adf123afad99
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-06-17 11:51:51 -0400

    Adding friends works

--- friends/src/components/AllFriends.js
+++ friends/src/components/AllFriends.js
@@ -2,39 +2,94 @@ import React, { useState, useEffect } from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
 function AllFriends(props) {
+	//State
 
-    let [friends, setFriends] = useState([{}]);
+	let [friends, setFriends] = useState([{}]);
 
-    useEffect(() => {
+	let [formInput, setFormInput] = useState({
+		name: '',
+		age: '',
+		email: '',
+	});
 
-        axiosWithAuth()
-        .get('/api/friends')
-        .then(response => {
-    
-            console.log(response.data);
+	//Functions
 
-            setFriends(response.data);
-    
-        })
-        .catch(error => console.log(error))
+	let changeInput = (event) => {
+		setFormInput({
+			...formInput,
+			[event.target.name]: event.target.value,
+		});
+	};
 
-    },[]);
+	useEffect(() => {
+		axiosWithAuth()
+			.get('/api/friends')
+			.then((response) => {
+				console.log(response.data);
 
-    return (
-        <div>
-            
-        {friends.map(item => {
+				setFriends(response.data);
+			})
+			.catch((error) => console.log(error));
+	}, []);
 
-            return (
-                <div>
-                    <p>{item.name}</p>
-                    <p>{item.age}</p>
-                    <p>{item.email}</p> 
-                </div>
-)})}
+	const addFriend = (event) => {
+		event.preventDefault();
+		axiosWithAuth()
+			.post('/api/friends', formInput)
+			.then((response) => {
+				console.log(response);
+				setFriends(response.data);
+			})
+			.catch((error) => console.log(error));
+	};
 
-        </div>
-    );
+	return (
+		<div>
+			<form onSubmit={addFriend}>
+				<label>
+					Name
+					<input
+						type="text"
+						onChange={changeInput}
+						name="name"
+						value={formInput.name}
+					></input>
+				</label>
+
+				<label>
+					Age
+					<input
+						type="text"
+						onChange={changeInput}
+						name="age"
+						value={formInput.age}
+					></input>
+				</label>
+
+				<label>
+					Email
+					<input
+						type="text"
+						onChange={changeInput}
+						name="email"
+						value={formInput.email}
+					></input>
+				</label>
+
+				<button>Add</button>
+			</form>
+
+			{friends.map((item) => {
+				return (
+					<div>
+						<p>{item.name}</p>
+						<p>{item.age}</p>
+						<p>{item.email}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default AllFriends;

78de5c9f86cd2ac895e3719af9c0fea44bb5d891
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-06-17 10:35:18 -0400

    Working login component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Login from './components/Login';
+import Login from "./components/Login";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-    <div className="App">
-     
-      <Route path='/'>
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Route path="/">
+					<h1>Not logged in.</h1>
 
-        <h1>Not logged in.</h1>
-
-        <Login />
-
-      </Route>
-
-
-    </div>
-    </BrowserRouter>
-  );
+					<Login />
+				</Route>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,48 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 function Login(props) {
+	//State
 
-    let [formInput, setFormInput] = useState({
-
-        username: '',
-        password: ''
-
+	let [formInput, setFormInput] = useState({
+		username: "",
+		password: "",
     });
+    
+    let history = useHistory();
 
-    let onChange = event => {
+	//Functions
 
-        setFormInput({
+	let changeInput = (event) => {
+		setFormInput({
+			...formInput,
+			[event.target.name]: event.target.value,
+		});
+	};
 
-            ...formInput,
-            [event.target.name]: event.target.value
+	let login = (event) => {
+		event.preventDefault();
 
-        })
+		axiosWithAuth()
+			.post('/api/login', formInput)
+			.then(response => {
 
-    }
+                console.log(response.data)
 
-    return (
+                window.localStorage.setItem('token', response.data.payload);
 
-        <form>
+                history.push('/dashboard');
 
-            <label>
-                Username
+            })
+			.catch(error => {console.log(error)});
+	};
 
-                <input type='text' onChange={onChange} name='username'></input>
+	return (
+		<form onSubmit={login}>
+			<label>
+				Username
+				<input
+					type="text"
+					onChange={changeInput}
+					name="username"
+					value={formInput.username}
+				></input>
+			</label>
 
-            </label>
+			<label>
+				Password
+				<input
+					type="password"
+					onChange={changeInput}
+					name="password"
+					value={formInput.password}
+				></input>
+			</label>
 
-            <label>
-                Password
-
-                <input type='password' onChange={onChange} name='password'></input>
-
-            </label>
-
-            <button>Login</button>
-
-        </form>
-        
-    );
+			<button>Login</button>
+		</form>
+	);
 }
 
 export default Login;
